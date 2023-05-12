43e08421d6ead0fbe823eb16c3692137d414f216
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2023-05-12 20:12:55 +0800

    Update FilesForLauncher.php

--- FilesForLauncher.php
+++ FilesForLauncher.php
@@ -11,7 +11,7 @@
   },
   {
     "path": "versions/SpookyClient/SpookyClient.json",
-    "checksumSHA1": "68a3bbbf413dba69ed7dcc42efdbb52b18fba7f4",
+    "checksumSHA1": "ccf02e90f8a8195eeae9b60458bbbfd489918412",
     "url": "https://spookykipper.github.io/SpookyClient/versions/SpookyClient/SpookyClient.json"
   },
   {
