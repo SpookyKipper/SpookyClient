8359e3675d71183af76091c9bffe9b3bbc5e9f58
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2023-05-12 20:08:02 +0800

    Update FilesForLauncher.php

--- FilesForLauncher.php
+++ FilesForLauncher.php
@@ -1,1 +1,23 @@
-[{"path":"SpookyClient.json","checksumSHA1":"ccf02e90f8a8195eeae9b60458bbbfd489918412","url":"https:\/\/spookykipper.github.io\/SpookyClient\/SpookyClient.json"},{"path":"SpookyClient.jar","checksumSHA1":"68a3bbbf413dba69ed7dcc42efdbb52b18fba7f4","url":"https:\/\/spookykipper.github.io\/SpookyClient\/SpookyClient.jar"},{"dirCheckUselessFiles":"mods"},{"dirCheckUselessFiles":"config"}]
+[
+  {
+    "path": "SpookyClient.json",
+    "checksumSHA1": "ccf02e90f8a8195eeae9b60458bbbfd489918412",
+    "url": "https://spookykipper.github.io/SpookyClient/SpookyClient.json"
+  },
+  {
+    "path": "SpookyClient.jar",
+    "checksumSHA1": "68a3bbbf413dba69ed7dcc42efdbb52b18fba7f4",
+    "url": "https://spookykipper.github.io/SpookyClient/SpookyClient.jar"
+  },
+  {
+    "path": "versions/SpookyClient/SpookyClient.json",
+    "checksumSHA1": "68a3bbbf413dba69ed7dcc42efdbb52b18fba7f4",
+    "url": "https://spookykipper.github.io/SpookyClient/versions/SpookyClient/SpookyClient.json"
+  },
+  {
+    "dirCheckUselessFiles": "mods"
+  },
+  {
+    "dirCheckUselessFiles": "config"
+  }
+]
